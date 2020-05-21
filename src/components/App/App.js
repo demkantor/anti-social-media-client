63aa594948ef5562fd6bcd284773996df6b886e7
demkantor <demkantor@gmail.com>
2020-05-20 21:21:46 -0500

    inital commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Anti-Social-Network</h1>
+    
+    </div>
+  );
+}
+
+export default App;
